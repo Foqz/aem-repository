1e86bcf1f85cf8f166b3a7c8d952f68f546f3a17
Foqz <fikskamil@gmail.com>
2020-03-23 12:42:09 +0100

    tab component

--- core/src/main/java/comd/adobe/aem/kamil/core/models/TabComponentModel.java
+++ core/src/main/java/comd/adobe/aem/kamil/core/models/TabComponentModel.java
@@ -4,6 +4,7 @@ import comd.adobe.aem.kamil.core.etc.TabDetails;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Optional;
 import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
@@ -17,19 +18,23 @@ import java.util.List;
 @Model(adaptables = Resource.class)
 public class TabComponentModel {
 
-    private List<TabDetails> tabDetailsList;
-
     @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
     private String tabContainerTitle;
+
     @SlingObject
     private Resource resource;
 
     @Inject
+    @Optional
     public Resource tabDetails;
+
+    private List<TabDetails> tabDetailsList;
 
     @PostConstruct
     public void init() {
-        tabDetailsList = getTabDetailsList(tabDetails);
+        if (tabDetails != null) {
+            tabDetailsList = getTabDetailsList(tabDetails);
+        }
     }
 
     public List<TabDetails> getTabDetailsList() {
@@ -46,9 +51,9 @@ public class TabComponentModel {
 
     private List<TabDetails> getTabDetailsList(Resource tabDetails) {
         List<TabDetails> tabDetailsList = new ArrayList<>();
-        Iterator<Resource> iterator = tabDetails.listChildren();
         List<Resource> childNodes = new ArrayList<>();
-        iterator.forEachRemaining(childNodes::add);
+        Iterator<Resource> resourceIterator = tabDetails.listChildren();
+        resourceIterator.forEachRemaining(childNodes::add);
         childNodes.forEach(r -> populateTabDetailsList(tabDetailsList, r.getValueMap()));
         return tabDetailsList;
     }
