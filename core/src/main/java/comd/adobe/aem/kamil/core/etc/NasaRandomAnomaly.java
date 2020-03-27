642237fadfb3d0bbbc55365e4c985529e6158a10
Foqz <fikskamil@gmail.com>
2020-03-27 11:46:31 +0100

    nasa component

---
+++ core/src/main/java/comd/adobe/aem/kamil/core/etc/NasaRandomAnomaly.java
@@ -0,0 +1,17 @@
+package comd.adobe.aem.kamil.core.etc;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class NasaRandomAnomaly {
+    private String copyright;
+    private String date;
+    private String explanation;
+    private String hdurl;
+    private String title;
+    private String url;
+}
---
+++ core/src/main/java/comd/adobe/aem/kamil/core/models/NasaComponentModel.java
@@ -0,0 +1,97 @@
+package comd.adobe.aem.kamil.core.models;
+
+import comd.adobe.aem.kamil.core.etc.NasaRandomAnomaly;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.commons.json.JSONException;
+import org.apache.sling.commons.json.JSONObject;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.LocalDate;
+
+@Model(adaptables = Resource.class)
+public class NasaComponentModel {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    private final String startingURL = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+
+    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+    private String componentTitle;
+    @ValueMapValue(injectionStrategy = InjectionStrategy.OPTIONAL)
+    private String anomalyDate;
+
+    private NasaRandomAnomaly nasaRandomAnomaly;
+
+    @PostConstruct
+    public void init() {
+        nasaRandomAnomaly = generateRandomAnomaly();
+    }
+
+    private NasaRandomAnomaly generateRandomAnomaly() {
+        StringBuilder response = new StringBuilder();
+        try {
+            String URL = generateUrl();
+            HttpURLConnection httpClient = (HttpURLConnection) new URL(URL).openConnection();
+            httpClient.setRequestMethod("GET");
+            httpClient.setRequestProperty("User-Agent", "Mozilla/5.0");
+            int responseCode = httpClient.getResponseCode();
+            logger.debug("\nSending 'GET' request to URL : " + URL);
+            logger.debug("Response Code : " + responseCode);
+            BufferedReader in = new BufferedReader(new InputStreamReader(httpClient.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                response.append(line);
+            }
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+        return populateRandomAnomaly(nasaRandomAnomaly, response.toString());
+    }
+
+    private String generateUrl() {
+        if (anomalyDate != null) {
+            StringBuilder urlBuilder = new StringBuilder();
+            String date = anomalyDate.substring(0, anomalyDate.indexOf("T"));
+            LocalDate dateToCheck = LocalDate.parse(date);
+            if (dateToCheck.compareTo(LocalDate.now()) > 0) {
+                return startingURL;
+            }
+            urlBuilder.append(startingURL).append("&date=").append(date);
+            return urlBuilder.toString();
+        }
+        return startingURL;
+    }
+
+    private NasaRandomAnomaly populateRandomAnomaly(NasaRandomAnomaly nasaRandomAnomaly, String response) {
+        NasaRandomAnomaly tempNasaRandomAnomaly = new NasaRandomAnomaly();
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            tempNasaRandomAnomaly.setTitle(jsonObject.getString("title"));
+            tempNasaRandomAnomaly.setUrl(jsonObject.getString("url"));
+            tempNasaRandomAnomaly.setCopyright(jsonObject.getString("copyright"));
+            tempNasaRandomAnomaly.setDate(jsonObject.getString("date"));
+            tempNasaRandomAnomaly.setExplanation(jsonObject.getString("explanation"));
+            tempNasaRandomAnomaly.setHdurl(jsonObject.getString("hdurl"));
+        } catch (JSONException e) {
+            logger.error(e.getMessage());
+        }
+        return tempNasaRandomAnomaly;
+    }
+
+    public NasaRandomAnomaly getNasaRandomAnomaly() {
+        return nasaRandomAnomaly;
+    }
+
+    public String getComponentTitle() {
+        return componentTitle;
+    }
+}
