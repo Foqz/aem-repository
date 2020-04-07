d1fe3766eb8bd957ce31059090a49cf17c70dddb
Foqz <fikskamil@gmail.com>
2020-04-07 11:21:08 +0200

    test dependencies + test class for chuck norris

---
+++ core/src/test/java/comd/adobe/aem/kamil/core/models/ChuckNorrisModelTest.java
@@ -0,0 +1,28 @@
+package comd.adobe.aem.kamil.core.models;
+
+
+import org.junit.jupiter.api.Test;
+
+public class ChuckNorrisModelTest {
+
+
+    @Test
+    public void getResponseFromURL() {
+    }
+
+    @Test
+    public void getComponentTitle() {
+    }
+
+    @Test
+    public void getFactCategories() {
+    }
+
+    @Test
+    public void isFactCategory() {
+    }
+
+    @Test
+    public void isFactTextSearch() {
+    }
+}
